8929f9544b4c5dd04169b2f449259113e6c882ad
CSFatimaAJ <csfatimaaljaber@icloud.com>
2022-11-03 11:21:28 +0300

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by Fatima Aljaber on 03/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
